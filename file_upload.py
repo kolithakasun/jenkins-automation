bc75c50673dd79e60fac285f2c6b78319800a362
Kolitha Dasanayaka <kolithad@hsenidmobile.com>
2023-04-07 22:34:37 +0530

    Adding test file

--- file_upload.py
+++ file_upload.py
@@ -27,7 +27,7 @@ def upload_to_sharepoint(dir_name: str, file_name: str):
     sp_relative_url = create_sharepoint_directory(dir_name)
     ctx = get_sharepoint_context_using_user()
     target_folder = ctx.web.get_folder_by_server_relative_url(sp_relative_url)
-    with open(file_name, 'rb') as content_file:
+    with open(f'uploads/{file_name}', 'rb') as content_file:
         file_content = content_file.read()
         target_folder.upload_file(file_name, file_content).execute_query()
 
