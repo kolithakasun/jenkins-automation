47bc66f4fafc710cd016eeceaa33b63c11c85cb1
Kolitha Dasanayaka <kolithad@hsenidmobile.com>
2023-04-07 23:11:21 +0530

    Bug fix

--- file_upload.py
+++ file_upload.py
@@ -9,14 +9,14 @@ def get_sharepoint_context_using_user():
     password = sys.argv[2]
     sharepoint_url = 'https://mahamalage.sharepoint.com'
     user_credentials = UserCredential(username,password)
-    ctx = ClientContext(sharepoint_url).with_credentials(user_credentials)
-    return ctx
+    context = ClientContext(sharepoint_url).with_credentials(user_credentials)
+    return context
 
 
 def create_sharepoint_directory(dir_name: str):
     if dir_name:
-        ctx = get_sharepoint_context_using_user()
-        result = ctx.web.folders.add(f'Shared Documents/{dir_name}').execute_query()
+        context = get_sharepoint_context_using_user()
+        result = context.web.folders.add(f'Shared Documents/{dir_name}').execute_query()
         if result:
             # documents is titled as Shared Documents for relative URL in SP
             relative_url = f'Shared Documents/{dir_name}'
@@ -25,8 +25,8 @@ def create_sharepoint_directory(dir_name: str):
 
 def upload_to_sharepoint(dir_name: str, file_name: str):
     sp_relative_url = create_sharepoint_directory(dir_name)
-    ctx = get_sharepoint_context_using_user()
-    target_folder = ctx.web.get_folder_by_server_relative_url(sp_relative_url)
+    context = get_sharepoint_context_using_user()
+    target_folder = context.web.get_folder_by_server_relative_url(sp_relative_url)
     with open(f'uploads/{file_name}', 'rb') as content_file:
         file_content = content_file.read()
         target_folder.upload_file(file_name, file_content).execute_query()
